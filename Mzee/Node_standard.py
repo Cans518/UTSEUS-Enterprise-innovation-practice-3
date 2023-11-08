901b421fbe66baed6c182fe3ca8f8f57cb2239e6
Mzee <798061533@qq.com>
2023-11-09 00:16:49 +0800

    完成Mzee版本的书写

--- Mzee/Node_standard.py
+++ Mzee/Node_standard.py
@@ -1,7 +1,14 @@
 import cv2
 import numpy as np
 import tkinter as tk
+from tkinter import simpledialog
 from tkinter import filedialog
+import ctypes
+
+# 设置DPI感知
+ctypes.windll.shcore.SetProcessDpiAwareness(1)
+
+invalid_time = simpledialog.askinteger("输入无效时长", "请输入无效时长，单位为秒")
 
 # 创建一个文件选择弹窗
 root = tk.Tk()
@@ -9,7 +16,10 @@ root.withdraw()  # 隐藏主窗口
 
 # 弹出文件选择对话框
 # file_path = filedialog.askopenfilename()
-file_path = "output.mp4"
+file_path = "temp/output.mp4"
+
+start_time = 0
+end_time = 0
 
 if not file_path:
     print("未选择视频文件")
@@ -17,14 +27,14 @@ else:
     # 打开视频文件
     cap = cv2.VideoCapture(file_path)
 
-    # 定义红色的颜色范围
-    lower_red = np.array([0, 0, 100])
-    upper_red = np.array([100, 100, 255])
+    # 定义血肉颜色的HSV范围，浅粉色到肉色到红色
+    lower_red = np.array([160, 100, 100])
+    upper_red = np.array([179, 255, 255])
 
     # 初始化一个标志变量，用于标记是否发现了血红色
     red_detected = False
     red_time_periods = []
-    threshold = 100  # 调整阈值以适应您的视频
+    threshold = 10  # 调整阈值以适应视频
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -40,7 +50,6 @@ else:
         # 计算遮罩中白色像素的数量
         white_pixel_count = np.sum(mask == 255)
 
-        # 如果白色像素数量足够多，认为发现了血红色
         if white_pixel_count > threshold:
             if not red_detected:
                 red_detected = True
@@ -49,12 +58,25 @@ else:
             if red_detected:
                 red_detected = False
                 end_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
-                red_time_periods.append((start_time, end_time))
+                if end_time - start_time > 30 and start_time > invalid_time:
+                    red_time_periods.append((start_time, end_time))
 
     # 释放视频捕捉对象
     cap.release()
 
-    # 打开一个 .inf 文件来写入时间段
-    with open('cat_time.in', 'w') as file:
+    # 打开一个 .in 文件来写入时间段
+    with open('temp/cat_time.in', 'w') as file:
         for start, end in red_time_periods:
-            file.write(f"{start}秒 - {end}秒\n")
+            # 输出的时候保留两位小数
+            file.write(f"{start:.2f} - {end:.2f}\n")
+        # 关闭文件
+        file.close()
+
+    # 重新打开 .in 文件来删除文件最后一行
+    with open('temp/cat_time.in', 'r+') as file:
+        lines = file.readlines()
+        file.seek(0)
+        file.truncate()
+        for line in lines[:-1]:
+            file.write(line)
+        file.close()
--- Mzee/video_cut.py
+++ Mzee/video_cut.py
@@ -1,70 +1,47 @@
 import subprocess
-import  sys
-import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog
-from tkinter import messagebox
+import sys
+import os
 
+def merge_time_ranges(time_ranges):
+  merged_time_ranges = []
+  for i in range(len(time_ranges)):
+    if i == 0:
+      merged_time_ranges.append(time_ranges[i])
+      continue
 
-def update_progress_bar(progress_bar, current_value, total_value):
-    progress = (current_value / total_value) * 100
-    progress_bar["value"] = progress
-    root.update()
+    previous_time_range = merged_time_ranges[-1]
+    current_time_range = time_ranges[i]
 
+    if previous_time_range[1] + 30 >= current_time_range[0]:
+      merged_time_ranges[-1] = (previous_time_range[0], current_time_range[1])
+    else:
+      merged_time_ranges.append(current_time_range)
 
-# 创建一个Tkinter窗口
-root = tk.Tk()
-root.withdraw()
+  return merged_time_ranges
 
-# 选择视频文件
-# input_video = filedialog.askopenfilename(title="选择视频文件")
-input_video = sys.argv[1]
+# 检查文件目录下output文件夹是否存在，不存在则创建
+if not os.path.exists("output"):
+    os.mkdir("output")
 
-if input_video:
-    # 选择片段信息文件
-    #info_file_path = filedialog.askopenfilename(title="选择片段信息文件 (cattime.inf)")
+def cut_video(input_file, segments):
+    i = 1
+    for start_time, end_time in segments:
+        output_file = f"output/cut{i}.mp4"
+        i += 1
+        command = f"ffmpeg -ss {start_time} -t {end_time - start_time} -i {input_file} -c copy {output_file} -y"
+        subprocess.call(command, shell=True)
 
-    info_file_path = "cat_time.in"
-
-    if info_file_path:
-        # 读取片段信息并存储在一个列表中
-        with open(info_file_path, "r") as info_file:
-            segments = [line.strip().split() for line in info_file]
-
-        # 创建弹窗
-        progress_window = tk.Toplevel()
-        progress_window.title("处理进度")
-
-        # 创建进度条
-        progress_bar = ttk.Progressbar(progress_window, mode="determinate", length=300)
-        progress_bar.grid(row=0, column=0, padx=10, pady=10)
-
-        total_segments = len(segments)
-
-        for i, (start_time, end_time) in enumerate(segments, start=1):
-            output_file = f"{i}.mp4"
-
-            # 使用FFmpeg来截取片段
-            command = [
-                "ffmpeg",
-                "-ss", start_time,  # 起始时间
-                "-i", input_video,  # 输入视频
-                "-to", end_time,  # 结束时间
-                "-c:v", "libx264",  # 视频编码器
-                "-c:a", "aac",  # 音频编码器
-                output_file  # 输出文件名
-            ]
-
-            subprocess.run(command)
-
-            # 更新进度条和弹窗信息
-            update_progress_bar(progress_bar, i, total_segments)
-            progress_window.title(f"处理进度 - {i}/{total_segments} 完成")
-
-        progress_window.destroy()
-
-        # 使用弹窗提示用户
-        messagebox.showinfo("提示", "所有片段已截取并保存。")
-
-# 关闭Tkinter窗口
-root.destroy()
+if __name__ == "__main__":
+    input_file = sys.argv[1]
+    info_file_path = "temp/cat_time.in"
+    segments = []
+    with open(info_file_path, "r") as info_file:
+        for line in info_file:
+            strat, end = line.strip().split(" - ")
+            segments.append((float(strat), float(end)))
+    segments = merge_time_ranges(segments)
+    cut_video(input_file, segments)
+    with open('temp\cat_time_fact.in', 'w') as file:
+        for start, end in segments:
+            # 输出的时候保留两位小数
+            file.write(f"{start:.2f} - {end:.2f}\n")
